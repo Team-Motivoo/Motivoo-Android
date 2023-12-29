f7a0483aceab51bbdb1c3eafa1d58bebdb1e61a5
l2zh <dlwnsgml0109@naver.com>
2023-12-30 01:21:07 +0900

    [init] #1 Firebase Analytics, Crashlytics 연동

--- app/src/main/java/sopt/motivoo/presentation/MainActivity.kt
+++ app/src/main/java/sopt/motivoo/presentation/MainActivity.kt
@@ -10,5 +10,6 @@ import sopt.motivoo.util.binding.BindingActivity
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
     }
 }
