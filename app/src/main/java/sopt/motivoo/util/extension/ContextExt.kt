b7dafd31c936f879c87a05cc24bfd998a3a14468
l2zh <dlwnsgml0109@naver.com>
2023-12-31 14:50:49 +0900

    [init] #1 스낵바 확장함수 추가

--- app/src/main/java/sopt/motivoo/util/extension/ContextExt.kt
+++ app/src/main/java/sopt/motivoo/util/extension/ContextExt.kt
@@ -4,18 +4,38 @@ import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
+import coil.Coil
+import coil.request.ImageRequest
+import com.google.android.material.snackbar.Snackbar
 
 fun Context.showToast(msg: String) {
     Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 }
 
+fun Context.showSnackbar(view: View, message: String, isShort: Boolean = true) {
+    val duration = if (isShort) Snackbar.LENGTH_SHORT else Snackbar.LENGTH_LONG
+    Snackbar.make(view, message, duration).show()
+}
+
 fun Context.colorOf(@ColorRes resId: Int) = ContextCompat.getColor(this, resId)
 
 fun Context.drawableOf(@DrawableRes resId: Int) = ContextCompat.getDrawable(this, resId)
+
+fun Context.loadingImage(imageUrl: String, imageView: ImageView, loadingImage: Int) {
+    imageUrl.let { url ->
+        val imageRequest = ImageRequest.Builder(this)
+            .data(url)
+            .placeholder(loadingImage)
+            .target(imageView)
+            .build()
+        Coil.imageLoader(this).enqueue(imageRequest)
+    }
+}
 
 fun Context.hideKeyboard(view: View) {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
--- app/src/main/java/sopt/motivoo/util/extension/ViewExt.kt
+++ app/src/main/java/sopt/motivoo/util/extension/ViewExt.kt
@@ -1,16 +1,6 @@
 package sopt.motivoo.util.extension
 
 import android.view.View
-import com.google.android.material.snackbar.Snackbar
-
-fun showSnackbar(view: View, message: String, isShort: Boolean = true) {
-    val duration = if (isShort) {
-        Snackbar.LENGTH_SHORT
-    } else {
-        Snackbar.LENGTH_LONG
-    }
-    Snackbar.make(view, message, duration).show()
-}
 
 fun View.setVisible(visibility: Int) {
     this.visibility = visibility
