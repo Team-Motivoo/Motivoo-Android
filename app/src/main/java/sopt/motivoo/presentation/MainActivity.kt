f8faa3f2aabddddb72e03a3fe0b10e246466dc52
l2zh <dlwnsgml0109@naver.com>
2024-01-01 21:57:28 +0900

    [feat] #3 jetpack navigation 구현 (임시 바텀 네비 추가)

--- app/src/main/java/sopt/motivoo/presentation/MainActivity.kt
+++ app/src/main/java/sopt/motivoo/presentation/MainActivity.kt
@@ -1,6 +1,10 @@
 package sopt.motivoo.presentation
 
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import sopt.motivoo.R
 import sopt.motivoo.databinding.ActivityMainBinding
@@ -10,5 +14,33 @@ import sopt.motivoo.util.binding.BindingActivity
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.fc_main)?.findNavController()
+
+        with(binding) {
+            bnvMain.itemIconTintList = null
+            navController?.let { navController ->
+                bnvMain.setupWithNavController(navController)
+            }
+        }
+        navController?.let { setBottomVisible(it) }
+    }
+
+    private fun setBottomVisible(navController: NavController) {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bnvMain.visibility = if (destination.id in listOf(
+                    R.id.splashFragment,
+                    R.id.loginFragment,
+                )
+            ) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
+        }
     }
 }
