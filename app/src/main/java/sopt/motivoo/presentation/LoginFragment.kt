e3c9eda814492deddcd0ea6d942f7af5402a21f4
l2zh <dlwnsgml0109@naver.com>
2024-01-01 21:28:02 +0900

    [feat] #3 프래그먼트 추가

---
+++ app/src/main/java/sopt/motivoo/presentation/LoginFragment.kt
@@ -0,0 +1,13 @@
+package sopt.motivoo.presentation
+
+import android.os.Bundle
+import android.view.View
+import sopt.motivoo.R
+import sopt.motivoo.databinding.FragmentLoginBinding
+import sopt.motivoo.util.binding.BindingFragment
+
+class LoginFragment : BindingFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/sopt/motivoo/presentation/SplashFragment.kt
@@ -0,0 +1,14 @@
+package sopt.motivoo.presentation
+
+import android.os.Bundle
+import android.view.View
+import sopt.motivoo.R
+import sopt.motivoo.databinding.FragmentSplashBinding
+import sopt.motivoo.util.binding.BindingFragment
+
+class SplashFragment : BindingFragment<FragmentSplashBinding>(R.layout.fragment_splash) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
