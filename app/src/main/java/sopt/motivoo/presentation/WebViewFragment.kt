25e1646d94940b48fb572de7a51f561a13ffc9c4
l2zh <dlwnsgml0109@naver.com>
2024-01-03 01:48:45 +0900

    [mod] #5 웹뷰프래그먼트 수정

--- app/src/main/java/sopt/motivoo/presentation/WebViewFragment.kt
+++ app/src/main/java/sopt/motivoo/presentation/WebViewFragment.kt
@@ -2,9 +2,6 @@ package sopt.motivoo.presentation
 
 import android.os.Bundle
 import android.view.View
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import sopt.motivoo.R
 import sopt.motivoo.databinding.FragmentWebviewBinding
 import sopt.motivoo.util.binding.BindingFragment
@@ -18,22 +15,7 @@ class WebViewFragment : BindingFragment<FragmentWebviewBinding>(R.layout.fragmen
     }
 
     private fun initWebView() {
-        binding.webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(
-                view: WebView,
-                request: WebResourceRequest
-            ): Boolean {
-                view.loadUrl(request.url.toString())
-                return true
-            }
-
-            override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view, url)
-            }
-        }
-
         binding.webView.settings.apply {
-            javaScriptEnabled = true
             loadWithOverviewMode = true
             useWideViewPort = true
             domStorageEnabled = true
