b8e465ea88e9bc56ef6c6771301f9d99c2fd6d9f
l2zh <dlwnsgml0109@naver.com>
2024-01-07 00:37:50 +0900

    [feat] #20 온보딩 페이지 생성

---
+++ app/src/main/java/sopt/motivoo/presentation/onboarding/DoExerciseQuestionFragment.kt
@@ -0,0 +1,9 @@
+package sopt.motivoo.presentation.onboarding
+
+import sopt.motivoo.R
+import sopt.motivoo.databinding.FragmentDoExerciseQusetionBinding
+import sopt.motivoo.util.binding.BindingFragment
+
+class DoExerciseQuestionFragment :
+    BindingFragment<FragmentDoExerciseQusetionBinding>(R.layout.fragment_do_exercise_qusetion) {
+}
