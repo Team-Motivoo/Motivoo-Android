0c0c10a1a9d77af39861afc8e72bd501889f32b6
l2zh <dlwnsgml0109@naver.com>
2024-01-07 01:54:32 +0900

    [feat] #20 온보딩 뷰 추가

---
+++ app/src/main/java/sopt/motivoo/presentation/onboarding/FrequencyActivityQuestionFragment.kt
@@ -0,0 +1,9 @@
+package sopt.motivoo.presentation.onboarding
+
+import sopt.motivoo.R
+import sopt.motivoo.databinding.FragmentFrequencyActivityQusetionBinding
+import sopt.motivoo.util.binding.BindingFragment
+
+class FrequencyActivityQuestionFragment :
+    BindingFragment<FragmentFrequencyActivityQusetionBinding>(R.layout.fragment_frequency_activity_qusetion) {
+}
---
+++ app/src/main/java/sopt/motivoo/presentation/onboarding/WhatActivityQuestionFragment.kt
@@ -0,0 +1,10 @@
+package sopt.motivoo.presentation.onboarding
+
+import sopt.motivoo.R
+import sopt.motivoo.databinding.FragmentWhatActivityQuestionBinding
+import sopt.motivoo.databinding.FragmentWhatExerciseQusetionBinding
+import sopt.motivoo.util.binding.BindingFragment
+
+class WhatActivityQuestionFragment :
+    BindingFragment<FragmentWhatActivityQuestionBinding>(R.layout.fragment_what_activity_question) {
+}
