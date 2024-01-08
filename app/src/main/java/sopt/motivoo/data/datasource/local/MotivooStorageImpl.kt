9e36afcd7f299cf9a30a23946f98766aae4d1b14
Jokwanhee <jonotch1@naver.com>
2024-01-08 21:09:49 +0900

    [add] #18 step_count property

--- app/src/main/java/sopt/motivoo/data/datasource/local/MotivooStorageImpl.kt
+++ app/src/main/java/sopt/motivoo/data/datasource/local/MotivooStorageImpl.kt
@@ -39,9 +39,14 @@ class MotivooStorageImpl @Inject constructor(@ApplicationContext context: Contex
         get() = pref.getString(EMAIL, "") ?: ""
         set(value) = pref.edit { putString(EMAIL, value) }
 
+    override var stepCount: Int
+        get() = pref.getInt(STEP_COUNT, 0)
+        set(value) = pref.edit { putInt(STEP_COUNT, 0) }
+
     companion object {
         private const val FILE_NAME = "MtDataStore"
         private const val NAME = "name"
         private const val EMAIL = "email"
+        private const val STEP_COUNT = "step_count"
     }
 }
