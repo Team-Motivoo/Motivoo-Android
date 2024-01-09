461fb5b6ee3aa051d934fd61e538094e663d5643
Jokwanhee <jonotch1@naver.com>
2024-01-09 20:51:32 +0900

    [del] #18 SeekBar

--- app/src/main/java/sopt/motivoo/presentation/home/HomeFragment.kt
+++ app/src/main/java/sopt/motivoo/presentation/home/HomeFragment.kt
@@ -54,7 +54,7 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
         }
 
         /**
-         * XXX 삭제 XXX
+         * 삭제될 것
          * 제공되는 미션 더미 처리
          */
         binding.motivooFirstMissionCard.apply {
@@ -65,20 +65,11 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
             setMissionImage(R.drawable.ic_clap_sound)
             setMissionText("8천걸음 걸어서 트리 보러가기")
         }
-
         /**
-         * XXX 삭제 XXX
-         * 원형 프로그래스 바 동작하는 거 보려고 SeekBar 만들어 놓았습니다.
+         * 삭제될 것
+         * 파이 차트 원형 프로그래스 바
          */
-        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                binding.motivooStepCountText.setStepCountText(p1.toString())
-                binding.motivooPieChart.setStepCount(((p1 / 100.0) * 320).toFloat())
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {}
-            override fun onStopTrackingTouch(p0: SeekBar?) {}
-        })
+        binding.motivooPieChart.setStepCount(((pref.stepCount / 100.0) * 320).toFloat())
     }
 
     private fun homePermissionsGranted() = HOME_REQUIRED_PERMISSIONS.all {
@@ -105,7 +96,6 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
     }
 
     override fun onDestroy() {
-        super.onDestroy()
         requireContext().unregisterReceiver(stepCountReceiver)
     }
 
