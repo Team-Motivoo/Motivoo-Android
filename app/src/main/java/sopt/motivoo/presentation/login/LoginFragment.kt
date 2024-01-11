828eacb0e96cef24ea8398077fde24c17c3fcae2
l2zh <dlwnsgml0109@naver.com>
2024-01-11 19:46:02 +0900

    [feat] #33 카카오 로그인 api 연동

--- app/src/main/java/sopt/motivoo/presentation/login/LoginFragment.kt
+++ app/src/main/java/sopt/motivoo/presentation/login/LoginFragment.kt
@@ -2,13 +2,25 @@ package sopt.motivoo.presentation.login
 
 import android.os.Bundle
 import android.view.View
+import dagger.hilt.android.AndroidEntryPoint
 import sopt.motivoo.R
+import sopt.motivoo.data.service.KakaoAuthService
 import sopt.motivoo.databinding.FragmentLoginBinding
 import sopt.motivoo.util.binding.BindingFragment
+import sopt.motivoo.util.extension.setOnSingleClickListener
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class LoginFragment : BindingFragment<FragmentLoginBinding>(R.layout.fragment_login) {
 
+    @Inject
+    lateinit var kakaoAuthService: KakaoAuthService
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.llKakaoLogin.setOnSingleClickListener {
+            kakaoAuthService.startKakaoLogin { platformType, platformToken, email, password, nickname ->
+                // 로그인 성공 시 처리할 로직
+            }
+        }
     }
 }
