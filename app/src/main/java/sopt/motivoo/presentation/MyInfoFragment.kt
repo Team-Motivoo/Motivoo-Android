264797bb96fd44828178869f81ac6cd9daabacb1
hyunjium <umhj0807@khu.ac.kr>
2024-01-11 22:01:33 +0900

    [feat] #24 dialog 버튼 및 텍스트 사이즈 변경

--- app/src/main/java/sopt/motivoo/presentation/MyInfoFragment.kt
+++ app/src/main/java/sopt/motivoo/presentation/MyInfoFragment.kt
@@ -7,10 +7,8 @@ import sopt.motivoo.R
 import sopt.motivoo.databinding.FragmentMypageMyinfoBinding
 import sopt.motivoo.util.binding.BindingFragment
 
-
 class MyInfoFragment :
     BindingFragment<FragmentMypageMyinfoBinding>(R.layout.fragment_mypage_myinfo) {
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
