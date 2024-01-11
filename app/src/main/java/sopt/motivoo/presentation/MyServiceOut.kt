b119024f6f2fe751c5460f2d508f8306902deb08
hyunjium <umhj0807@khu.ac.kr>
2024-01-12 01:54:14 +0900

    [feat] #24 base 다이얼로그 프래그먼트 적용

--- app/src/main/java/sopt/motivoo/presentation/MyServiceOut.kt
+++ app/src/main/java/sopt/motivoo/presentation/MyServiceOut.kt
@@ -1,38 +1,16 @@
 package sopt.motivoo.presentation
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
 import sopt.motivoo.R
 import sopt.motivoo.databinding.FragmentMypageServiceOutBinding
+import sopt.motivoo.util.binding.BindingDialogFragment
 
-class MyServiceOut : DialogFragment() {
-
-    private lateinit var binding: FragmentMypageServiceOutBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        binding = FragmentMypageServiceOutBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
+class MyServiceOut :
+    BindingDialogFragment<FragmentMypageServiceOutBinding>(R.layout.fragment_mypage_service_out) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val width = ViewGroup.LayoutParams.MATCH_PARENT
-        val height = ViewGroup.LayoutParams.MATCH_PARENT
-
-        dialog?.window?.setLayout(width, height)
-        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
         clickButton()
     }
 
--- app/src/main/java/sopt/motivoo/util/binding/BindingDialogFragment.kt
+++ app/src/main/java/sopt/motivoo/util/binding/BindingDialogFragment.kt
@@ -15,6 +15,7 @@ import sopt.motivoo.R
 abstract class BindingDialogFragment<T : ViewDataBinding>(
     @LayoutRes private val layoutRes: Int,
 ) : DialogFragment() {
+    val a = 0
     private var _binding: T? = null
     protected val binding get() = _binding ?: error(getString(R.string.binding_error))
 
@@ -24,13 +25,20 @@ abstract class BindingDialogFragment<T : ViewDataBinding>(
         savedInstanceState: Bundle?,
     ): View {
         _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        forDialogSideMargin()
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    private fun forDialogSideMargin() {
         val width = ViewGroup.LayoutParams.MATCH_PARENT
         val height = ViewGroup.LayoutParams.MATCH_PARENT
 
         dialog?.window?.setLayout(width, height)
-        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        return binding.root
     }
 
     override fun onDestroyView() {
