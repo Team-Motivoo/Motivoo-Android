56bde1bab718867ca17e3d72737d69485ecd102f
hyunjium <umhj0807@khu.ac.kr>
2024-01-17 02:07:08 +0900

    [feat] #47 마이_내정보 뷰 서버 연결

--- app/src/main/java/sopt/motivoo/presentation/MyInfoFragment.kt
+++ app/src/main/java/sopt/motivoo/presentation/MyInfoFragment.kt
@@ -2,16 +2,25 @@ package sopt.motivoo.presentation
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import sopt.motivoo.R
+import sopt.motivoo.data.service.MyPageService
 import sopt.motivoo.databinding.FragmentMypageMyinfoBinding
+import sopt.motivoo.presentation.mypage.MyPageViewModel
 import sopt.motivoo.util.binding.BindingFragment
 
 class MyInfoFragment :
     BindingFragment<FragmentMypageMyinfoBinding>(R.layout.fragment_mypage_myinfo) {
+
+    private val myPageViewModel by viewModels<MyPageViewModel>()
+    lateinit var myPageService: MyPageService
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        myPageViewModel.getUserInfo()
+        binding.tvMyinfoUserName.text = myPageViewModel.myPageResult.value?.userNickname
+        binding.tvMyinfoUserAge.text = myPageViewModel.myPageResult.value?.userAge.toString()
         clickButtons()
     }
 
