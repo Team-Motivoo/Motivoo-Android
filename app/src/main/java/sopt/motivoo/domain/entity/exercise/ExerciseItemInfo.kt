6b39ff0c06bd12307a1cd626794dd5f2f6919006
giovannijunseokim <kingjunseo@gmail.com>
2024-01-17 23:53:17 +0900

    [feat] #50 ExerciseItemInfo rename 및 ExerciseData 내부로 옮김

---
+++ app/src/main/java/sopt/motivoo/domain/entity/exercise/ExerciseItemInfo.kt
@@ -0,0 +1,23 @@
+package sopt.motivoo.domain.entity.exercise
+
+class ExerciseData(
+    val userType: String,
+    val exerciseItemInfoList: List<ExerciseItemInfo>,
+) {
+    sealed class ExerciseItemInfo {
+        data class EachDateItemInfo(
+            val date: String,
+            val myMissionImgUrl: String?,
+            val opponentMissionImgUrl: String?,
+            val myMissionContent: String,
+            val myMissionStatus: String,
+            val opponentMissionContent: String,
+            val opponentMissionStatus: String,
+        ) : ExerciseItemInfo()
+
+        data class NoticeItemInfo(
+            val missionContent: String,
+        ) : ExerciseItemInfo()
+    }
+
+}
