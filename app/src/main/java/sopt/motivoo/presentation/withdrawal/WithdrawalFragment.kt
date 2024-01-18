c9fb93ed44a233d4661d3c762417659623e3bd5f
giovannijunseokim <kingjunseo@gmail.com>
2024-01-19 07:48:19 +0900

    [feat] #76 WithdrawalFragment init

---
+++ app/src/main/java/sopt/motivoo/presentation/withdrawal/WithdrawalFragment.kt
@@ -0,0 +1,11 @@
+package sopt.motivoo.presentation.withdrawal
+
+import sopt.motivoo.R
+import sopt.motivoo.databinding.FragmentWithdrawalBinding
+import sopt.motivoo.util.binding.BindingFragment
+
+class WithdrawalFragment :
+    BindingFragment<FragmentWithdrawalBinding>(R.layout.fragment_withdrawal) {
+
+
+}
