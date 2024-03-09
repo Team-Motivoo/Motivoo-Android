845832717ebc2dc7b3f91be9c44d0910996adc28
Jokwanhee <jonotch1@naver.com>
2024-03-09 19:11:29 +0900

    [mod] #102 HomeRepository

--- app/src/main/java/sopt/motivoo/domain/entity/onboarding/GetMatchedInfo.kt
+++ app/src/main/java/sopt/motivoo/domain/entity/onboarding/GetMatchedInfo.kt
@@ -1,5 +1,7 @@
 package sopt.motivoo.domain.entity.onboarding
 
 data class GetMatchedInfo(
+    val userId: Int,
+    val opponentUserId: Int,
     val isMatched: Boolean
 )
--- app/src/main/java/sopt/motivoo/domain/repository/HomeRepository.kt
+++ app/src/main/java/sopt/motivoo/domain/repository/HomeRepository.kt
@@ -1,19 +1,15 @@
 package sopt.motivoo.domain.repository
 
 import android.graphics.Bitmap
-import sopt.motivoo.data.model.request.home.RequestMissionImageDto
-import sopt.motivoo.data.model.request.home.RequestMissionTodayDto
 import sopt.motivoo.domain.entity.home.HomeData
 import sopt.motivoo.domain.entity.home.MissionChoiceData
 import sopt.motivoo.domain.entity.home.MissionImageData
 
 interface HomeRepository {
-    fun getMyStepCount(uid: String, isInitStepCount: (Boolean) -> Unit)
-    fun getOtherStepCount(otherUid: String, onStepCountAction: (Long) -> Unit)
-    fun setMyStepCount(uid: String)
-    suspend fun patchHome(myStepCount: Int, otherStepCount: Int): Result<HomeData>
-    suspend fun postMissionTodayChoice(): Result<MissionChoiceData>
-    suspend fun postMissionToday(requestMissionTodayDto: RequestMissionTodayDto): Result<Unit>
-    suspend fun patchMissionImage(requestMissionImageDto: RequestMissionImageDto): Result<MissionImageData>
-    suspend fun uploadPhoto(url: String, bitmap: Bitmap): Result<Unit>
+    suspend fun patchHome(myStepCount: Int, otherStepCount: Int): HomeData?
+    suspend fun postMissionTodayChoice(): MissionChoiceData?
+    suspend fun postMissionToday(missionId: Int): Unit?
+    suspend fun getMissionImage(imagePrefix: String): MissionImageData?
+    suspend fun patchMissionImage(fileName: String): Unit?
+    suspend fun uploadPhoto(url: String, bitmap: Bitmap): Unit?
 }
