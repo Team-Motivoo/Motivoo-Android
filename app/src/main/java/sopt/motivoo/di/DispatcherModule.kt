90e64894052b1332929812913a1cb8c55f198215
Jokwanhee <jonotch1@naver.com>
2024-03-09 19:10:57 +0900

    [add] #102 repository & dispatcher 모듈

---
+++ app/src/main/java/sopt/motivoo/di/DispatcherModule.kt
@@ -0,0 +1,21 @@
+package sopt.motivoo.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class DefaultDispatcher
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DispatcherModule {
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+}
