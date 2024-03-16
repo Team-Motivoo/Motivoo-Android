968ae2ffb7d54b5b9491d5b0acf20c4ded2ea27d
Jokwanhee <jonotch1@naver.com>
2024-03-17 03:59:46 +0900

    [add] #118 ktlint check

--- app/src/main/java/sopt/motivoo/di/DispatcherModule.kt
+++ app/src/main/java/sopt/motivoo/di/DispatcherModule.kt
@@ -6,7 +6,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import javax.inject.Qualifier
 
 @InstallIn(SingletonComponent::class)
 @Module
