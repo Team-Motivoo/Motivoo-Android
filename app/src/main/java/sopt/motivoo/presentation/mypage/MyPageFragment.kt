6a19b722cc598dd097482be9f9c0b1071b0faa07
hyunjium <umhj0807@khu.ac.kr>
2024-03-19 09:02:50 +0900

    [fix] #115 사용 안하는 import 삭제

--- app/src/main/java/sopt/motivoo/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/sopt/motivoo/presentation/mypage/MyPageFragment.kt
@@ -3,7 +3,6 @@ package sopt.motivoo.presentation.mypage
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
